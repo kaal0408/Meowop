2b2938e47f1400fa1f108f924ddc4e6be946cb90
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-03-10 15:32:13 +0530

    Add files via upload

---
+++ Destroy.py
@@ -0,0 +1,15 @@
+import logging
+from os import system
+
+# Logs
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
+)
+# Starting bot
+system("git clone https://github.com/Criminal786/DESTROYX && cd DESTROYX && python3 -m userbot")
+# if got error
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @DESTROYXsupport")
+# Script written for stop heroku suspension
+
+
+#Don't Try To Steal Without Credit @Simpleboy786
