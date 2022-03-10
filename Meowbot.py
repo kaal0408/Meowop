4006b178322be3099cc0d27731851b8921903362
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-03-10 15:33:39 +0530

    Update and rename Destroy.py to Meowbot.py

---
+++ Meowbot.py
@@ -0,0 +1,15 @@
+import logging
+from os import system
+
+# Logs
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
+)
+# Starting bot
+system("git clone https://github.com/kaal0408/MeowUserbot && cd MeowUserbot && python3 -m Meowbot")
+# if got error
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @MeowUserbot")
+# Script written for stop heroku suspension
+
+
+#Don't Try To Steal Without Credit @Murat_30_God
